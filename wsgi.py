3e91e65de828ac7a5a8f28e22fc1776f05a6245c
Dave Reed <dave.reed@thomsonreuters.com>
2017-05-14 22:37:52 -0400

    added Procfile for Heroku

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,5 @@
-import sys
-sys.path.insert(0, '/home/dreed/webapps/flask01')
+# import sys
+# sys.path.insert(0, '/home/dreed/webapps/flask01')
 #sys.path.insert(0, '/home/dreed/webapps/flask02')
 from flask01 import app as application
 #from flask02 import app as application
