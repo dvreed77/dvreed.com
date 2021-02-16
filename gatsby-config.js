960451262915829145c89f5004e45ed52f7bca71
Dave Reed <dave.reed@thomsonreuters.com>
2021-02-15 20:30:20 -0500

    reorganized directory  structure

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,7 +27,7 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `posts`,
-        path: `${__dirname}/src/posts/`,
+        path: `${__dirname}/content/posts/`,
       },
     },
     {
