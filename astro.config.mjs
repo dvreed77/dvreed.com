f7646aa692578b5938e231c526e6f15852acec7c
Dave Reed <david.reed.c@gmail.com>
2022-11-11 22:09:05 -0500

    added react tailwind and first blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,28 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+import mdx from "@astrojs/mdx";
+
+// https://astro.build/config
+import react from "@astrojs/react";
+
+// https://astro.build/config
+export default defineConfig({
+  markdown: {
+    shikiConfig: {
+      // Choose from Shiki's built-in themes (or add your own)
+      // https://github.com/shikijs/shiki/blob/main/docs/themes.md
+      theme: "min-light",
+      // Add custom languages
+      // Note: Shiki has countless langs built-in, including .astro!
+      // https://github.com/shikijs/shiki/blob/main/docs/languages.md
+      langs: [],
+      // Enable word wrap to prevent horizontal scrolling
+      wrap: true
+    }
+  },
+  integrations: [tailwind(), mdx(), react()]
+});
---
+++ src/components/SimpleTable.tsx
@@ -0,0 +1,48 @@
+export const SimpleTable = () => {
+  const data = [
+    { city: "Portland", state: "Oregon", count: "80" },
+    { city: "Denver", state: "Colorado", count: "77" },
+    { city: "San Diego", state: "California", count: "74" },
+    { city: "Chicago", state: "Illinois", count: "69" },
+    { city: "Seattle", state: "Washington", count: "67" },
+    { city: "Austin", state: "Texas", count: "43" },
+    { city: "Albuquerque", state: "New Mexico", count: "40" },
+    { city: "San Francisco", state: "California", count: "37" },
+    { city: "Minneapolis", state: "Minnesota", count: "36" },
+    { city: "Indianapolis", state: "Indiana", count: "35" },
+    { city: "Asheville", state: "North Carolina", count: "30" },
+    { city: "Los Angeles", state: "California", count: "29" },
+    { city: "Milwaukee", state: "Wisconsin", count: "28" },
+    { city: "Brooklyn", state: "New York", count: "26" },
+    { city: "Saint Louis", state: "Missouri", count: "26" },
+    { city: "Cincinnati", state: "Ohio", count: "26" },
+    { city: "Nashville", state: "Tennessee", count: "25" },
+    { city: "Columbus", state: "Ohio", count: "25" },
+    { city: "Colorado Springs", state: "Colorado", count: "25" },
+  ];
+
+  return (
+    <div className="overflow-x-auto">
+      <table className="table w-full">
+        <thead>
+          <tr>
+            <th></th>
+            <th>City</th>
+            <th>State</th>
+            <th>Count</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map(({ city, state, count }, i) => (
+            <tr>
+              <th>{i}</th>
+              <td>{city}</td>
+              <td>{state}</td>
+              <td>{count}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
