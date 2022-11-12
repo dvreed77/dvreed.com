bdc7241274e554351e119d9a62ad86ef5df3e0b4
Dave Reed <david.reed.c@gmail.com>
2022-11-12 00:14:27 -0500

    plotting functions

--- src/components/DistributionViewer.tsx
+++ src/components/DistributionViewer.tsx
@@ -1,57 +1,46 @@
 import React from "react";
-// import * as d3 from "d3";
-// import {
-//   VictoryChart,
-//   VictoryLine,
-//   VictoryAxis,
-//   VictoryLabel,
-//   VictoryScatter,
-//   VictoryArea,
-//   VictoryContainer,
-// } from "victory";
 import { random2 } from "./randomF";
-
-function ScatterLine({ x, y, ...other }: { [key: string]: any }) {
-  const yRange = other.scale.y.range();
-  return (
-    <line
-      x1={x}
-      x2={x}
-      y1={yRange[0]}
-      y2={yRange[1]}
-      stroke={other.style.fill}
-      opacity={other.style.opacity}
-    />
-  );
-}
+import { VegaLite, VisualizationSpec } from "react-vega";
 
 interface IProps {
-  f: (x: number) => number;
+  f: string;
   className: string;
 }
 
-export function DistributionViewer({ f = (x) => 1, className }: IProps) {
-  const [dimensions, setDimensions] = React.useState({
-    width: 800,
-    height: 200,
-  });
-
-  const [pdfData, setPdfData] = React.useState([]);
-  const [cdfData, setCdfData] = React.useState([]);
+const funcs: { [k: string]: (x: number) => number } = {
+  f1: (x: number) => 1,
+  f2: (x) => x,
+  fGaussian: (x) => Math.exp(-0.5 * ((x - 0.5) / 0.1) ** 2),
+  f3: (x) => x ** 3,
+  f4: (x) => x ** 3 + 0.2,
+  f5: (x) => x ** 2 + (1 - x) ** 10,
+  f6: (x) => (x > 0 ? x / x ** 2 : 0),
+  f7: (x) => 1 + Math.cos(2 * Math.PI * x),
+  f8: (x) => 1 + Math.cos(2 * Math.PI * x + Math.PI),
+};
+export function DistributionViewer({ f = "f1", className }: IProps) {
+  const [pdfData, setPdfData] = React.useState<[number, number][]>([]);
+  const [cdfData, setCdfData] = React.useState<[number, number][]>([]);
   const [randomData, setRandomData] = React.useState([1, 2, 3]);
 
-  // const [scales, setScales] = React.useState({
-  //   xScale: d3.scaleLinear(),
-  //   yScale: d3.scaleLinear(),
-  // });
-
-  const [params, setParams] = React.useState({
-    start: 0.5,
-    end: 0.5,
-  });
-
+  const f2 = funcs[f];
   React.useEffect(() => {
-    const rGen = new random2(f);
+    // if (!f) return;
+    const rGen = new random2(f2);
+    // const nPts = 1000
+    // const dx = 1 / (nPts - 1)
+
+    // const pdfData: [number, number][] = Array.from({
+    //   length: nPts,
+    // }).map((_, idx) => [idx * dx, f(idx * dx)])
+
+    // const cdfData: [number, number][] = pdfData
+    //   .slice(1)
+    //   .reduce((a, b) => [...a, [b[0], a[a.length - 1][1] + dx * b[1]]], [
+    //     [0, 0],
+    //   ])
+
+    // const maxD = d3.max(cdfData, d => d[1])
 
     setPdfData(rGen.pdfData);
     setCdfData(rGen.cdfData);
@@ -64,109 +53,25 @@ export function DistributionViewer({ f = (x) => 1, className }: IProps) {
     setRandomData(r);
   }, []);
 
-  // React.useEffect(() => {
-  //   setScales({
-  //     xScale: d3.scaleLinear().domain([0, 1]).range([0, dimensions.width]),
-  //     yScale: d3.scaleLinear().domain([0, 1]).range([dimensions.height, 0]),
-  //   });
-  // }, [dimensions]);
+  const spec: VisualizationSpec = {
+    width: 1000,
+    height: 30,
+    mark: "rule",
+    encoding: {
+      x: { field: "x", type: "quantitative", scale: { domain: [0, 1] } },
+      color: { value: "black" },
+      opacity: { value: 0.4 },
+      size: { value: 0.2 },
+    },
+    data: { name: "table" }, // note: vega-lite data attribute is a plain object instead of an array
+    config: {
+      axis: { domain: false, grid: false, ticks: false, disable: true },
+    },
+  };
 
-  return (
-    <div className={className}>
-      <div className="flex flex-row flex-wrap justify-center">
-        <div>
-          {/* <VictoryChart
-            height={200}
-            width={300}
-            domainPadding={{ y: 5 }}
-            containerComponent={<VictoryContainer responsive={false} />}
-          >
-            <VictoryLabel text="PDF" x={150} y={30} textAnchor="middle" />
-            <VictoryArea
-              style={{ data: { fill: "#bbb" } }}
-              data={pdfData}
-              x={(d) => d[0]}
-              y={(d) => d[1]}
-            />
-            <VictoryLine
-              x={(d) => d[0]}
-              y={(d) => d[1]}
-              data={pdfData}
-              style={{
-                data: {
-                  strokeWidth: 1,
-                },
-              }}
-            />
-            <VictoryAxis
-              tickLabelComponent={
-                <VictoryLabel style={{ textAnchor: "end", fontSize: "10px" }} />
-              }
-              style={{
-                axis: { stroke: "#756f6a" },
-                axisLabel: { fontSize: 20, padding: 30 },
-                ticks: { stroke: "grey", size: 5 },
-                tickLabels: { fontSize: 15, padding: 5 },
-              }}
-            />
-          </VictoryChart> */}
-        </div>
-        <div>
-          {/* <VictoryChart
-            height={200}
-            width={300}
-            domainPadding={{ y: 5 }}
-            containerComponent={<VictoryContainer responsive={false} />}
-          >
-            <VictoryLabel text="CDF" x={150} y={30} textAnchor="middle" />
-            <VictoryArea
-              style={{ data: { fill: "#bbb" } }}
-              data={cdfData}
-              x={(d) => d[0]}
-              y={(d) => d[1]}
-            />
-            <VictoryLine
-              x={(d) => d[0]}
-              y={(d) => d[1]}
-              data={cdfData}
-              style={{
-                data: {
-                  strokeWidth: 1,
-                },
-              }}
-            />
-            <VictoryAxis
-              tickLabelComponent={
-                <VictoryLabel style={{ textAnchor: "end", fontSize: "10px" }} />
-              }
-              style={{
-                axis: { stroke: "#756f6a" },
-                axisLabel: { fontSize: 20, padding: 30 },
-                ticks: { stroke: "grey", size: 5 },
-                tickLabels: { fontSize: 15, padding: 5 },
-              }}
-            />
-          </VictoryChart> */}
-        </div>
-        <div>
-          <svg width={300} height={80}>
-            {/* <VictoryLabel text="Samples" x={150} y={60} textAnchor="middle" />
-            <VictoryScatter
-              standalone={false}
-              x={(d) => d}
-              y={(d) => 1}
-              domain={{ y: [0, 2] }}
-              size={3}
-              style={{ data: { opacity: 0.1 } }}
-              data={randomData}
-              width={300}
-              height={50}
-              padding={10}
-              dataComponent={<ScatterLine />}
-            /> */}
-          </svg>
-        </div>
-      </div>
-    </div>
-  );
+  const barData = {
+    table: randomData.map((d) => ({ x: d })),
+  };
+
+  return <VegaLite spec={spec} data={barData} actions={false} />;
 }
