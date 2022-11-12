a2455a7153cddd81efb6973ac2dc328fe9488f12
Dave Reed <david.reed.c@gmail.com>
2022-11-12 13:54:29 -0500

    using picture instead of images

--- astro.config.mjs
+++ astro.config.mjs
@@ -24,14 +24,21 @@ export default defineConfig({
       // Add custom languages
       // Note: Shiki has countless langs built-in, including .astro!
       // https://github.com/shikijs/shiki/blob/main/docs/languages.md
-      langs: []
+      langs: [],
       // Enable word wrap to prevent horizontal scrolling
       // wrap: true,
     },
 
     remarkPlugins: [remarkMath],
     rehypePlugins: [rehypeKatex],
-    extendDefaultPlugins: true
+    extendDefaultPlugins: true,
   },
-  integrations: [tailwind(), mdx(), react(), image()]
+  integrations: [
+    tailwind(),
+    mdx(),
+    react(),
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp",
+    }),
+  ],
 });
