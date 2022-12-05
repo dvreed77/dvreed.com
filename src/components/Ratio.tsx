badeba4d87d0e48a8b22b2614f461cfed995d8e2
Dave Reed <david.reed.c@gmail.com>
2022-12-04 20:06:31 -0500

    made bayes look better

--- src/components/Ratio.tsx
+++ src/components/Ratio.tsx
@@ -1,11 +1,10 @@
 import { useEffect, useRef, useState } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTractor, faBookReader } from "@fortawesome/free-solid-svg-icons";
 import { atom } from "nanostores";
 import { useStore } from "@nanostores/react";
-import pkg from "react-katex";
+import * as katex from "react-katex";
+import { path } from "d3";
+const { BlockMath } = katex;
 
-const { BlockMath } = pkg;
 const myState = atom({ a: 0, b: 0, pH: 0.5 });
 
 const librarianColor = "#d97706";
@@ -13,33 +12,7 @@ const librarianColorLight = "#f59e0b";
 const farmerColor = "#059669";
 const farmerColorLight = "#10b981";
 
-export const Slider1 = () => {
-  const $myState = useStore(myState);
-
-  return (
-    <div className="flex flex-col mb-2">
-      <label
-        htmlFor="myRange"
-        className="italic text-sm font-thin text-gray-700"
-      >
-        Use the slider to adjust the ratio between librarians and farmers
-      </label>
-      <input
-        type="range"
-        min="-7"
-        max="7"
-        onChange={(e) => myState.set({ ...$myState, a: +e.target.value })}
-        // value={$myState.a}
-        // onChange={(e) => console.log(e.target.value)}
-        value={$myState.a}
-        step="1"
-        id="myRange"
-      />
-    </div>
-  );
-};
-
-export const Slider2 = () => {
+export const SliderH = () => {
   const $myState = useStore(myState);
   return (
     <div className="flex flex-row">
@@ -59,27 +32,7 @@ export const Slider2 = () => {
   );
 };
 
-export const Slider3 = () => {
-  const $myState = useStore(myState);
-  return (
-    <div className="flex flex-row">
-      <input
-        className="flex-auto"
-        type="range"
-        min="0"
-        max="1"
-        step="0.1"
-        onChange={(e) => myState.set({ ...$myState, b: +e.target.value })}
-        value={$myState.b}
-      />
-      <div className="ml-2">
-        <BlockMath math={`P(B|A) = ${$myState.b.toFixed(1)}`} />
-      </div>
-    </div>
-  );
-};
-
-export const Slider4 = () => {
+export const SliderA = () => {
   const $myState = useStore(myState);
   return (
     <div className="flex flex-row">
@@ -93,7 +46,27 @@ export const Slider4 = () => {
         value={$myState.a}
       />
       <div className="ml-2">
-        <BlockMath math={`P(B|\\bar{A}) = ${$myState.a.toFixed(1)}`} />
+        <BlockMath math={`P(B|A) = ${$myState.a.toFixed(1)}`} />
+      </div>
+    </div>
+  );
+};
+
+export const SliderB = () => {
+  const $myState = useStore(myState);
+  return (
+    <div className="flex flex-row">
+      <input
+        className="flex-auto"
+        type="range"
+        min="0"
+        max="1"
+        step="0.1"
+        onChange={(e) => myState.set({ ...$myState, b: +e.target.value })}
+        value={$myState.b}
+      />
+      <div className="ml-2">
+        <BlockMath math={`P(B|\\bar{A}) = ${$myState.b.toFixed(1)}`} />
       </div>
     </div>
   );
@@ -105,9 +78,6 @@ export const Square1 = () => {
   const $myState = useStore(myState);
 
   const { pH, a, b } = $myState;
-
-  const p1 = 0.5;
-  const pNotH = 1 - pH;
 
   useEffect(() => {
     const el = divRef.current;
@@ -133,26 +103,25 @@ export const Square1 = () => {
               height={svgSize}
               fill={farmerColor}
             />
-
             <rect
               x={0}
-              y={0}
+              y={svgSize * (1 - a)}
               width={pH * svgSize}
-              height={b * svgSize}
+              height={a * svgSize}
               fill={librarianColorLight}
             />
             <rect
               x={pH * svgSize}
-              y={0}
+              y={svgSize * (1 - b)}
               width={svgSize - pH * svgSize}
-              height={a * svgSize}
+              height={b * svgSize}
               fill={farmerColorLight}
             />
           </svg>
         </div>
       </div>
 
-      <div>
+      {/* <div>
         <BlockMath math={`P(H) = ${pH}`} />
         <BlockMath math={`P(E|H) = ${b}`} />
         <BlockMath math={`P(H)P(E|H) = ${(b * pH).toFixed(2)}`} />
@@ -160,140 +129,197 @@ export const Square1 = () => {
         <BlockMath
           math={`P(\\bar{H})P(E|\\bar{H}) = ${(a * (1 - pH)).toFixed(2)}`}
         />
-      </div>
+      </div> */}
     </>
   );
 };
-export function Ratio() {
-  const [value, setValue] = useState(0);
-  const [pH, setPH] = useState(0.5);
 
-  const [pNotH, setPNotH] = useState(0.5);
+const Bracket = ({
+  x1,
+  x2,
+  y1,
+  y2,
+}: {
+  x1: number;
+  x2: number;
+  y1: number;
+  y2: number;
+}) => {
+  const dx = x2 - x1;
+  const dy = y2 - y1;
+  const length = Math.sqrt(dx * dx + dy * dy);
+  const mid = length / 2;
+  const angle = Math.atan2(dy, dx);
 
-  const ratio = value > 0 ? [1, Math.abs(value) + 1] : [Math.abs(value) + 1, 1];
+  const w = 2;
+  const p = path();
+  p.moveTo(0, 0);
+  p.lineTo(w, -w);
+  p.lineTo(mid - w, -w);
+  p.lineTo(mid, -w * 2);
+  p.lineTo(mid + w, -w);
+  p.lineTo(length - w, -w);
+  p.lineTo(length, 0);
 
-  const p1 = ratio[0] ** 2 / (ratio[0] ** 2 + ratio[1] ** 2);
+  const d = p.toString();
 
   return (
-    <div className="mt-5">
-      <div style={{ minHeight: 250 }}>
-        <div className="flex">
-          <div className="flex flex-col mr-3 w-1/3">
-            <h4>
-              Librarians{" "}
-              <span className="text-gray-700 font-thin">({ratio[0] ** 2})</span>
-            </h4>
-            <div>
-              {Array.from({ length: ratio[0] }).map((_, idx) => (
-                <div className="flex flex-row" key={idx}>
-                  {Array.from({ length: ratio[0] }).map((_, idx) => (
-                    <FontAwesomeIcon
-                      key={idx}
-                      className="m-1"
-                      icon={faBookReader}
-                      color={librarianColor}
-                      size="xs"
-                    />
-                  ))}
-                </div>
-              ))}
-            </div>
-          </div>
-          <div className="flex flex-col ml-5 w-1/3">
-            <h4>
-              Farmers{" "}
-              <span className="text-gray-700 font-thin">({ratio[1] ** 2})</span>
-            </h4>
-            <div>
-              {Array.from({ length: ratio[1] }).map((_, idx) => (
-                <div className="flex flex-row" key={idx}>
-                  {Array.from({ length: ratio[1] }).map((_, idx) => (
-                    <FontAwesomeIcon
-                      key={idx}
-                      className="m-1"
-                      icon={faTractor}
-                      color="#2A9D8F"
-                      size="xs"
-                    />
-                  ))}
-                </div>
-              ))}
-            </div>
-          </div>
-          <div className="w-1/3">
-            <div className="flex flex-row italic items-center text-3xl">
-              <span>P</span>
-              <span>(</span>
-              <span style={{ color: "#264653" }}>H</span>
-              <span>)</span>
-              <span>=</span>
-              <span>
-                {(ratio[0] ** 2 / (ratio[0] ** 2 + ratio[1] ** 2)).toFixed(3)}
-              </span>
-            </div>
-            <p className="italic text-sm font-thin text-gray-700">
-              The probability Steve is a librarian before you saw any
-              evidence... the <span className="font-semibold">Prior</span>
-            </p>
-          </div>
-        </div>
-      </div>
-      <hr />
-      <div className="mb-3">
-        <h2>
-          What is the Probability of Seeing this Evidence if Steve was a
-          Librarian?
-        </h2>
-        <span className="text-sm font-light">
-          Thinking of any interactions you have had with librarians, what
-          percentage of librarians do you think match this description?
-        </span>
+    <path
+      d={d}
+      stroke={"#ccc"}
+      fill="none"
+      transform={`translate(${x1}, ${y1}) rotate(${(angle * 180) / Math.PI})`}
+    />
+  );
+};
+export const Square2 = () => {
+  const divRef = useRef<HTMLDivElement>(null);
+  const [svgSize, setSvgSize] = useState(0);
+  const $myState = useStore(myState);
 
-        <div className="flex flex-row italic items-center text-2xl">
-          <span>P</span>
-          <span>(</span>
-          <span>E</span> <span>|</span>{" "}
-          <span style={{ color: librarianColor }}>H</span> <span>)</span>
-          <span>=</span>
-          <span>{pH}</span>
-        </div>
-      </div>
-      <hr />
-      <div className="mb-3">
-        <h2>
-          What is the Probability of Seeing this Evidence if Steve was a Farmer?
-        </h2>
-        <span className="text-sm font-light">
-          Thinking of any interactions you have had with farmers, what
-          percentage of farmers do you think match this description?
-        </span>
-        <div className="flex flex-col mt-2 mb-2">
-          <label className="italic text-sm font-thin text-gray-700">
-            Use the slider to adjust the percentage
-          </label>
-          <input
-            className="w-1/3"
-            type="range"
-            min="0"
-            max="1"
-            step="0.1"
-            onChange={(e) => setPNotH(+e.target.value)}
-            value={pNotH}
-          />
-        </div>
-        <div className="flex flex-row italic items-center text-2xl">
-          <span>P</span>
-          <span>(</span>
-          <span>E</span> <span>|</span>{" "}
-          <span style={{ color: farmerColor }}>¬H</span> <span>)</span>
-          <span>=</span>
-          <span>{pNotH}</span>
-        </div>
-      </div>
-      <hr />
-      <div>
-        <h2>Putting it all together</h2>
+  const { pH, a } = $myState;
+
+  useEffect(() => {
+    const el = divRef.current;
+    setSvgSize((el?.clientWidth ?? 0) / 5);
+  }, []);
+
+  const svgWidth = 800;
+  const s = svgSize - 60;
+  const width = pH * s;
+  const height = Math.max(a * s, 5);
+
+  return (
+    <div ref={divRef}>
+      <div className="flex flex-col justify-center">
+        <svg width={svgWidth} height={svgSize} className="mx-auto">
+          <g transform={`translate(${svgWidth / 2}, ${svgSize / 2})`}>
+            <rect
+              x={-width / 2}
+              y={-height / 2}
+              width={width}
+              height={height}
+              fill={librarianColorLight}
+            />
+            <Bracket
+              x1={-width / 2 - 4}
+              x2={-width / 2 - 4}
+              y1={height / 2}
+              y2={-height / 2}
+            />
+
+            <Bracket
+              x1={-width / 2}
+              x2={width / 2}
+              y1={-height / 2 - 4}
+              y2={-height / 2 - 4}
+            />
+
+            <text
+              x={-width / 2 - 20}
+              textAnchor="end"
+              fontSize={15}
+              alignmentBaseline="central"
+            >
+              P(B|A) = {a.toFixed(2)}
+            </text>
+            <text
+              y={-height / 2 - 20}
+              textAnchor="middle"
+              fontSize={15}
+              alignmentBaseline="central"
+            >
+              P(A) = {pH.toFixed(2)}
+            </text>
+            <text
+              y={height / 2 + 20}
+              textAnchor="middle"
+              fontSize={15}
+              alignmentBaseline="central"
+            >
+              P(A)P(B|A) = {(pH * a).toFixed(2)}
+            </text>
+          </g>
+        </svg>
       </div>
     </div>
   );
-}
+};
+
+export const Square3 = () => {
+  const divRef = useRef<HTMLDivElement>(null);
+  const [svgSize, setSvgSize] = useState(0);
+  const $myState = useStore(myState);
+
+  const { pH, b } = $myState;
+
+  useEffect(() => {
+    const el = divRef.current;
+    setSvgSize((el?.clientWidth ?? 0) / 5);
+  }, []);
+
+  const svgWidth = 800;
+  const s = svgSize - 60;
+
+  // const width = svgSize - pH * svgSize;
+  const width = (1 - pH) * s;
+  // const height = b * svgSize;
+  const height = Math.max(b * s, 5);
+
+  return (
+    <div ref={divRef}>
+      <div className="flex flex-col justify-center">
+        <svg width={svgWidth} height={svgSize} className="mx-auto">
+          <g transform={`translate(${svgWidth / 2}, ${svgSize / 2})`}>
+            <rect
+              x={-width / 2}
+              y={-height / 2}
+              width={width}
+              height={height}
+              fill={farmerColorLight}
+            />
+
+            <Bracket
+              x1={-width / 2 - 4}
+              x2={-width / 2 - 4}
+              y1={height / 2}
+              y2={-height / 2}
+            />
+
+            <Bracket
+              x1={-width / 2}
+              x2={width / 2}
+              y1={-height / 2 - 4}
+              y2={-height / 2 - 4}
+            />
+
+            <text
+              x={-width / 2 - 20}
+              textAnchor="end"
+              fontSize={15}
+              alignmentBaseline="central"
+            >
+              P(B|Ā) = {b.toFixed(2)}
+            </text>
+            <text
+              y={-height / 2 - 20}
+              textAnchor="middle"
+              fontSize={15}
+              alignmentBaseline="central"
+            >
+              P(Ā) = {(1 - pH).toFixed(2)}
+            </text>
+            <text
+              y={height / 2 + 20}
+              textAnchor="middle"
+              fontSize={15}
+              alignmentBaseline="central"
+            >
+              P(Ā)P(B|Ā) = {((1 - pH) * b).toFixed(2)}
+            </text>
+          </g>
+        </svg>
+      </div>
+    </div>
+  );
+};
