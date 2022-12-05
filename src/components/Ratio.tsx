98b0cc51c9b6ce2c0ba2dea92dd18ef2035cb516
Dave Reed <david.reed.c@gmail.com>
2022-12-04 20:14:34 -0500

    fixed build

--- src/components/Ratio.tsx
+++ src/components/Ratio.tsx
@@ -25,9 +25,9 @@ export const SliderH = () => {
         onChange={(e) => myState.set({ ...$myState, pH: +e.target.value })}
         value={$myState.pH}
       />
-      <div className="ml-2">
+      {/* <div className="ml-2">
         <BlockMath math={`P(H) = ${$myState.pH.toFixed(1)}`} />
-      </div>
+      </div> */}
     </div>
   );
 };
@@ -45,9 +45,9 @@ export const SliderA = () => {
         onChange={(e) => myState.set({ ...$myState, a: +e.target.value })}
         value={$myState.a}
       />
-      <div className="ml-2">
+      {/* <div className="ml-2">
         <BlockMath math={`P(B|A) = ${$myState.a.toFixed(1)}`} />
-      </div>
+      </div> */}
     </div>
   );
 };
@@ -65,9 +65,9 @@ export const SliderB = () => {
         onChange={(e) => myState.set({ ...$myState, b: +e.target.value })}
         value={$myState.b}
       />
-      <div className="ml-2">
+      {/* <div className="ml-2">
         <BlockMath math={`P(B|\\bar{A}) = ${$myState.b.toFixed(1)}`} />
-      </div>
+      </div> */}
     </div>
   );
 };
