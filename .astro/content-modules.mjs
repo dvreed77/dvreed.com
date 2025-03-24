9930e3279357557e42e46620756933a66db7cae9
Dave Reed <david.reed.c@gmail.com>
2025-03-24 11:07:59 -0400

    fixed README

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,11 +1,11 @@
 
 export default new Map([
-["src/content/posts/bayes.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fbayes.mdx&astroContentModuleFlag=true")],
-["src/content/posts/random_generators.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Frandom_generators.mdx&astroContentModuleFlag=true")],
-["src/content/posts/thomson_sampling.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fthomson_sampling.mdx&astroContentModuleFlag=true")],
-["src/content/posts/breweries.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fbreweries.mdx&astroContentModuleFlag=true")],
 ["src/content/projects/boston-landfill.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fboston-landfill.mdx&astroContentModuleFlag=true")],
 ["src/content/projects/kmap.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fkmap.mdx&astroContentModuleFlag=true")],
+["src/content/projects/mortgage-calculator.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fmortgage-calculator.mdx&astroContentModuleFlag=true")],
+["src/content/posts/bayes.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fbayes.mdx&astroContentModuleFlag=true")],
+["src/content/posts/breweries.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fbreweries.mdx&astroContentModuleFlag=true")],
 ["src/content/posts/math_test.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fmath_test.mdx&astroContentModuleFlag=true")],
-["src/content/projects/mortgage-calculator.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fmortgage-calculator.mdx&astroContentModuleFlag=true")]]);
+["src/content/posts/thomson_sampling.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fthomson_sampling.mdx&astroContentModuleFlag=true")],
+["src/content/posts/random_generators.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Frandom_generators.mdx&astroContentModuleFlag=true")]]);
 		
