523261be17abdd99c84fddccee09220f6c47d1e7
Dave Reed <david.reed.c@gmail.com>
2025-03-24 09:29:59 -0400

    sort of working

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,39 +1,22 @@
 import { defineConfig } from "astro/config";
-
-// https://astro.build/config
 import tailwind from "@astrojs/tailwind";
-
-// https://astro.build/config
 import mdx from "@astrojs/mdx";
-
-// https://astro.build/config
 import react from "@astrojs/react";
-import rehypeKatex from "rehype-katex";
 import remarkMath from "remark-math";
 
-// https://astro.build/config
 export default defineConfig({
+  integrations: [
+    mdx({
+      optimize: false,
+      remarkPlugins: [remarkMath]
+    }),
+    react(),
+    tailwind()
+  ],
   markdown: {
     shikiConfig: {
-      // Choose from Shiki's built-in themes (or add your own)
-      // https://github.com/shikijs/shiki/blob/main/docs/themes.md
       theme: "min-light",
-      // Add custom languages
-      // Note: Shiki has countless langs built-in, including .astro!
-      // https://github.com/shikijs/shiki/blob/main/docs/languages.md
-      langs: [],
-      // Enable word wrap to prevent horizontal scrolling
-      // wrap: true,
-    },
-
-    remarkPlugins: [remarkMath],
-    rehypePlugins: [rehypeKatex],
-    extendDefaultPlugins: true,
-  },
-  integrations: [
-    tailwind(),
-    mdx(),
-    react(),
-
-  ],
+      langs: []
+    }
+  }
 });
--- src/content/config.ts
+++ src/content/config.ts
@@ -8,6 +8,19 @@ const posts = defineCollection({
   }),
 });
 
+const projects = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    startDate: z.string(),
+    endDate: z.string(),
+    repoURL: z.string().optional(),
+    projectURL: z.string().optional(),
+    blurb: z.string(),
+    images: z.array(z.string()),
+  }),
+});
+
 export const collections = {
   posts,
+  projects,
 };
